58034c8e2597f72945a580a76ffa1093576252d1
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-14 17:53:11 +0700

    Add interface AwsVpcCreatorStackProps

---
+++ lib/AwsVpcCreatorStackProps.ts
@@ -0,0 +1,20 @@
+import { StackProps } from "aws-cdk-lib";
+
+export interface AwsVpcCreatorStackProps extends StackProps {
+    /**
+     * The name of the application.
+     */
+    readonly appName: string;
+    /**
+     * Prefix used for naming AWS resources.
+     */
+    readonly resourcePrefix: string;
+    /**
+     * The environment where the CDK stack will be deployed.
+     */
+    readonly cdkDeployEnvironment: string;
+    /**
+     * The AWS region where the stack will be deployed.
+     */
+    readonly cdkDeployRegion: string;
+}
