5650a4655760a777367f70f54eece173d0b7d593
KennyDizi <xamarindevelopervietnam@outlook.com>
2024-04-14 17:57:47 +0700

    Create vpc and flow logs

--- lib/aws-vpc-creator-stack.ts
+++ lib/aws-vpc-creator-stack.ts
@@ -1,4 +1,8 @@
 import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import * as logs from 'aws-cdk-lib/aws-logs';
+import { RetentionDays } from 'aws-cdk-lib/aws-logs';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 import { AwsVpcCreatorStackProps } from './AwsVpcCreatorStackProps';
 
@@ -6,11 +10,69 @@ export class AwsVpcCreatorStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props: AwsVpcCreatorStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const removalPolicy = props.cdkDeployEnvironment === 'production' ? cdk.RemovalPolicy.RETAIN : cdk.RemovalPolicy.DESTROY;
+    // create vpc
+    const vpcName = `${props.resourcePrefix}-VPC`;
+    const awsVpc = new ec2.Vpc(this, vpcName, {
+            ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'), //IPs in Range - 65,536
+            natGateways: 3, // for high availability
+            maxAzs: 3, // for high availability
+            subnetConfiguration: [
+                {
+                    name: `${props.resourcePrefix}-PUBLIC`,
+                    subnetType: ec2.SubnetType.PUBLIC,
+                    cidrMask: 24, //IPs in Range - 256
+                },
+                {
+                    name: `${props.resourcePrefix}-PRIVATE_WITH_EGRESS`,
+                    subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS,
+                    cidrMask: 24, //IPs in Range - 256
+                },
+            ],
+            enableDnsHostnames: true,
+            enableDnsSupport: true,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsVpcCreatorQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // apply removal policy to all vpc and subnet resources
+    awsVpc.applyRemovalPolicy(removalPolicy);
+    for (const subnet of awsVpc.privateSubnets) {
+        subnet.applyRemovalPolicy(removalPolicy);
+    }
+    for (const subnet of awsVpc.isolatedSubnets) {
+        subnet.applyRemovalPolicy(removalPolicy);
+    }
+    for (const subnet of awsVpc.publicSubnets) {
+        subnet.applyRemovalPolicy(removalPolicy);
+    }
+
+    const vpcFlowLogRole = new iam.Role(this, `${props.resourcePrefix}-RoleVpcFlowLogs`, {
+        assumedBy: new iam.ServicePrincipal("vpc-flow-logs.amazonaws.com"),
+        managedPolicies: [
+            iam.ManagedPolicy.fromAwsManagedPolicyName("CloudWatchFullAccess"),
+        ],
+    });
+
+    const vpcFlowLogGroup = new logs.LogGroup(this, `${props.resourcePrefix}-VpcFlowLogGroup`, {
+        retention: RetentionDays.ONE_MONTH,
+        removalPolicy: removalPolicy,
+    });
+
+    new logs.LogStream(this, `${props.resourcePrefix}-VpcFlowLogStream`, {
+        logGroup: vpcFlowLogGroup,
+        removalPolicy: removalPolicy,
+    });
+
+    new ec2.FlowLog(this, `${props.resourcePrefix}-VpcFlowLog`, {
+        resourceType: ec2.FlowLogResourceType.fromVpc(awsVpc),
+        destination: ec2.FlowLogDestination.toCloudWatchLogs(vpcFlowLogGroup, vpcFlowLogRole),
+        trafficType: ec2.FlowLogTrafficType.ALL,
+    });
+
+    // print out vpc id
+    new cdk.CfnOutput(this, `${props.resourcePrefix}-VPC-ID-Export`, {
+        value: awsVpc.vpcId,
+        exportName: `${props.resourcePrefix}-VPC-ID-Export`,
+        description: 'Infrastructure VPC ID.',
+    });
   }
 }
